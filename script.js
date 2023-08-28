614971288030a04fcd109fdb80f438752eaf3ea8
donavenjb <beuccedonaven25@gmail.com>
2023-08-28 03:07:59 +0000

    Integrated email list feature and further  developing CRUD functions

--- script.js
+++ script.js
@@ -29,11 +29,10 @@ function addStudent() {
 
 function inputAreaClear() {
   var clearHtml = `
-    Select an action from the dropdown menu and click "Perform Action"   
+    Select an action from the dropdown menu and click "Perform Action"
   `;
   document.getElementById("inputArea").innerHTML = clearHtml;
 }
-
 
 function updateStudentInfo() {
   const testTest = 'You are testing update function, its working';
@@ -45,15 +44,28 @@ function showStudentInfo() {
   document.getElementById("outputArea").innerHTML = testTest;
 }
 
-function promptForStudentID() {
-  const getStudentId = `<input type="number" id="getStudentId" placeholder="Please enter student ID number"></input><br /><button onclick="proceedWithSearch()">Search</button>`;
+function promptForStudentID(selectedOption) {
+  const getStudentId = '<input type="number" id="getStudentId" placeholder="Please enter student ID number"></input><br /><button id="searchBtn">Search</button>';
   document.getElementById("outputArea").innerHTML = getStudentId;
+  const btn = document.getElementById("searchBtn");
+  btn.setAttribute("data-selected-option", selectedOption);
+  btn.addEventListener('click', function() {
+    proceedWithSearch(this.getAttribute("data-selected-option"));
+  });
 }
 
-function proceedWithSearch() {
+function proceedWithSearch(selectedOption) {
   const studentId = parseInt(document.getElementById("getStudentId").value);
-  if(searchStudent(studentId)) {
-    calculateGrade(studentId);
+  if (searchStudent(studentId)) {
+    switch (selectedOption) {
+      case 'C':
+        calculateGrade(studentId);
+        break;
+      // Add other cases as needed
+      default:
+        // Handle the default case
+        break;
+    }
   } else {
     const studentFoundFalse = 'Student ID is invalid';
     document.getElementById("outputArea").innerHTML = studentFoundFalse;
@@ -62,24 +74,21 @@ function proceedWithSearch() {
 
 function calculateGrade(studentId) {
   const studentIndex = student_ids.indexOf(studentId);
-  if(studentIndex !== -1) {
+  if (studentIndex !== -1) {
     const average_score = (test_score1[studentIndex] + test_score2[studentIndex] + test_score3[studentIndex]) / 3;
     document.getElementById("outputArea").innerHTML = "Average score for student ID " + studentId + " is: " + average_score;
-  } 
+  }
 }
 
 function searchStudent(studentId) {
-  for(let i = 0; i < student_ids.length; i++) {
-    
-    if(studentId === student_ids[i]) {
+  for (let i = 0; i < student_ids.length; i++) {
+    if (studentId === student_ids[i]) {
       const testId = 'student ID: ' + student_ids[i];
       return true;
     }
   }
   return false;
 }
-
-
 
 function performAction() {
   var selectedOption = document.getElementById("selectAction").value;
@@ -94,7 +103,7 @@ function performAction() {
       showStudentInfo();
       break;
     case 'C':
-      promptForStudentID();
+      promptForStudentID(selectedOption);
       break;
     default:
       document.getElementById('outputArea').innerHTML = "Invalid choice. Please try again!";
@@ -102,7 +111,64 @@ function performAction() {
   }
 }
 
+function initializeEmailList() {
+
+  const $ = selector => document.querySelector(selector);
+
+  const joinList = evt => {
+    evt.preventDefault();
+
+    const email1 = $("#email_1").value;
+    const email2 = $("#email_2").value;
+    const firstName = $("#first_name").value;
+
+    let isValid = true;
+    if (email1 == "") {
+      $("#email_1_error").textContent = "Email is required.";
+      isValid = false;
+    } else {
+      $("#email_1_error").textContent = "";
+    }
+
+    if (email1 != email2) {
+      $("#email_2_error").textContent = "Emails must match.";
+      isValid = false;
+    } else {
+      $("#email_2_error").textContent = "";
+    }
+
+    if (firstName == "") {
+      $("#first_name_error").textContent = "First name is required.";
+      isValid = false;
+    } else {
+      $("#first_name_error").textContent = "";
+    }
+
+    if (!isValid) {
+      evt.preventDefault();
+    }
+    document.getElementById('outputArea').innerHTML = "You've joined the list, let's do this";
+  };
+
+  const clearForm = () => {
+    $("#email_1").value = "";
+    $("#email_2").value = "";
+    $("#first_name").value = "";
+
+    $("#email_1_error").textContent = "*";
+    $("#email_2_error").textContent = "*";
+    $("#first_name_error").textContent = "*";
+
+    $("#email_1").focus();
+  };
+
+  $("#join_list").addEventListener("click", joinList);
+  $("#clear_form").addEventListener("click", clearForm);
+  $("#email_1").focus();
+}
+
 window.onload = function() {
   document.getElementById('inputArea').innerHTML = 'Select an action from the dropdown menu and click "Perform Action"';
   document.getElementById('outputArea').innerHTML = "Results will be displayed here.";
+  initializeEmailList();
 }
